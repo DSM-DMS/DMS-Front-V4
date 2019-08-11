beb6f16522c9f7609b612f49aa86369ecce31e7d
gtobio11 <gtobio11@gmail.com>
2019-08-11 14:00:18 +0900

    [MODIFY] <기본 설정> tsconfig 설정과 webpack 설정을 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,59 @@
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: ["@babel/polyfill", __dirname + "/src/index"],
+  entry: ['@babel/polyfill', `${__dirname}/src/index.tsx`],
   output: {
-    path: __dirname + "/dist",
-    filename: "[name].[hash].js",
-    publicPath: "/"
+    path: `${__dirname}/dist`,
+    filename: '[name].[hash].js',
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ['babel-loader'],
       },
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: ["ts-loader"]
+        use: ['ts-loader'],
       },
       {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
-          { loader: "style-loader" },
-          { loader: "css-loader" }
-        ]
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ],
       },
       {
         test: /\.(png|jpe?g|svg|gif)$/,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
-              limit: 100000
-            }
-          }
-        ]
-      }
-    ]
+              limit: 100000,
+            },
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx"]
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./public/index.html"
+      template: './public/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
   ],
-  devtool: "inline-source-map"
+  devtool: 'inline-source-map',
 };
