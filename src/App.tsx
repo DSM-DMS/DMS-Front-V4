80841b8f7a04d08b0ced6abee7b823fce791887a
gtobio11 <gtobio11@gmail.com>
2019-08-27 11:34:05 +0900

    [CREATE] <redux> redux 기반 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Provider } from 'react-redux';
 import { GlobalStyle } from './GlobalStyle';
 import { NavigationContainer } from './containers';
+import configureStore from './store/configureStore';
 
-const Test = styled.div`
-  background: red;
-`;
+const store = configureStore();
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <NavigationContainer />
-      <Test>test</Test>
-    </>
+    </Provider>
   );
 }
 
---
+++ src/modules/index.ts
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+export interface StoreState {}
+
+export default combineReducers<StoreState>({});
