e3ef4f4d1623e2996a7e1390392e0e6bd89323c9
unknown <leaf_cis@naver.com>
2019-08-30 13:53:23 +0900

    [MODIFY] <Header.tsx> 수정

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as S from './styles.ts'
+import * as S from './styles'
 import Logo from '../../assets/logo/logo.png'
 import { Link } from 'react-router-dom'; 
 
