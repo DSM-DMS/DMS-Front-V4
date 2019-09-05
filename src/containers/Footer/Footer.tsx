acf44c3c327e15d9422d8ac664eac9e7c5e387be
gtobio11 <gtobio11@gmail.com>
2019-09-05 16:02:59 +0900

    [ADD] <Footer Container> Team Column에 key값 부여

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -5,7 +5,9 @@ import { teamList } from '../../static/footer';
 interface FooterProps {}
 
 const FooterContainer: React.FC<FooterProps> = ({}) => {
-  const teamColumns = teamList.map(data => <FooterTeamColumn {...data} />);
+  const teamColumns = teamList.map((data, index) => (
+    <FooterTeamColumn {...data} key={`footer-${index}-list`} />
+  ));
   return <Footer>{teamColumns}</Footer>;
 };
 
