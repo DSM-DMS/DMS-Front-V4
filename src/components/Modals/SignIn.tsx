e9e3227b5463241cab4dae6a2aac210495de4c66
gimtommang11 <gimtommang88@gmail.com>
2019-09-10 01:04:15 +0900

    [CREATE]<components/Modals/SignIn.tsx>회원가입 파일 생성
    creat signin.tsx

---
+++ src/components/Modals/SignIn.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as S from './style';
+
+interface Props{
+}
+
+const SignIn: React.FC<Props> =()=> {
+  return(
+    <>
+    </>
+  );
+}
+
+export default SignIn;
