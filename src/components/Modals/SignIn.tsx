81490b94c64827f867dc3862719fdfb7b26a2ef6
gimtommang11 <gimtommang88@gmail.com>
2019-09-16 19:04:35 +0900

    [ADD]<components/modals/Signin.tsx>회원가입 퍼블리싱 완료
    회원가입 모달창 퍼블리싱 변경된 디자인으로 완료하였습니다

--- src/components/Modals/SignIn.tsx
+++ src/components/Modals/SignIn.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import ModalCloseImg from '../../assets/icon/blue_close-mark.png';
 import * as S from './style';
 
 interface Props{
@@ -7,6 +8,27 @@ interface Props{
 const SignIn: React.FC<Props> =()=> {
   return(
     <>
+      <S.ModalNameWrapper>
+        <S.ModalName>회원가입</S.ModalName>
+        <S.ModalClosebtn 
+          src={ModalCloseImg}/>
+      </S.ModalNameWrapper>
+
+      <S.SigninContentWrapper>
+        <S.ModalInput
+          placeholder="확인코드"/>
+        <S.ModalInput 
+          placeholder="아이디"/>
+        <S.ModalInput 
+          placeholder="비밀번호"
+          type="password"/>
+        <S.ModalInput
+          placeholder="비밀번호 확인"
+          type="password"/>
+      </S.SigninContentWrapper>
+
+
+      <S.button>회원가입</S.button>
     </>
   );
 }
