a5eae6dd9cb0a5f5a4cad9fc2ade48e3aa6ed1a0
SuHaLee <gimtommang88@gmail.com>
2019-09-23 23:47:58 +0900

    [ADD]<components/modals/ChangePW>비밀번호 변경 퍼블리싱 완료
    비밀번호 변경 페이지 펴블리싱 완료

---
+++ src/components/modals/ChangePW.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import ModalCloseImg from '../../assets/icon/black-close-button.png';
+import * as S from './style';
+
+interface Props{
+}
+
+const ChangePW: React.StatelessComponent<Props>=()=>{
+  return(
+    <>
+      <S.ModalNameWrapper>
+        <S.ModalName>비밀번호 변경</S.ModalName>
+        <S.ModalClosebtn 
+          src={ModalCloseImg}/>
+      </S.ModalNameWrapper>
+      <S.FindPWContentWrapper>
+        <S.ModalInput
+          placeholder="현재비밀번호"
+          type="password"/>
+        <S.ModalInput
+          placeholder="새로운 비밀번호"
+          type="password"/>
+        <S.ModalInput
+          placeholder="비밀번호 확인"
+          type="password"/>
+      </S.FindPWContentWrapper>
+      <S.ModalSubmitBtn>회원가입</S.ModalSubmitBtn>
+    </>
+  );
+}
+
+export default ChangePW;
