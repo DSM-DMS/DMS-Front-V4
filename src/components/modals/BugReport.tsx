68912791c14b66c1fbea974da7c979b0cc234f32
SuHaLee <gimtommang88@gmail.com>
2019-09-25 22:50:16 +0900

    [MODIFY]<components/modal/BugReport>버그리포트 모달 퍼블리싱 완성

---
+++ src/components/modals/BugReport.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import ModalCloseImg from '../../assets/icon/black-close-button.png';
+import BugIconImg from '../../assets/icon/bug.png'
+import * as S from './style';
+
+interface Props{
+}
+
+const BugReport: React.SFC<Props>=()=>{
+  return(
+    <>
+       <S.ModalNameWrapper>
+        <S.ModalName>
+          <S.BugIcon src={BugIconImg}/>
+          버그신고
+          </S.ModalName>
+        <S.ModalClosebtn 
+          src={ModalCloseImg}/>
+      </S.ModalNameWrapper>
+      <S.BugreportInput
+       placeholder="버그신고 내용을 입력해주세요"/>
+
+       <S.ModalSubmitBtn>신고하기</S.ModalSubmitBtn>
+    </>
+  );
+}
+
+export default BugReport;
