c546b67b951e753dce7428c657ccbfd10be46e2b
gtobio11 <gtobio11@gmail.com>
2019-10-03 21:22:09 +0900

    [MODIFY] <GlobalStyle> font cdn 위치 변경

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -25,10 +25,6 @@ time, mark, audio, video, button {
   padding: 0;
   border: 0;
 }
-body {
-  @import url('https://fonts.googleapis.com/css?family=Noto+Sans:400,700&display=swap')
-  font-family: 'Noto Sans', sans-serif;
-}
 
 input:focus,
 select:focus,
