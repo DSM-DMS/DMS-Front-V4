5274515f9eaacf8a40da394971a7e728aa872015
gtobio11 <gtobio11@gmail.com>
2019-10-03 16:55:05 +0900

    [MODIFY] <Notice Reducer, Containers> redux 구조 및 코드 변경

--- src/containers/NoticeTagsList/NoticeTagsList.tsx
+++ src/containers/NoticeTagsList/NoticeTagsList.tsx
@@ -1,42 +1,38 @@
 import React, { useState } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { noticeActions } from '../../modules/notice';
+import { useDispatch, useSelector } from 'react-redux';
+import { StoreState } from '../../modules';
+import { setSelectedTagThunk } from '../../modules/notice';
 import { NoticeTags } from '../../static/notice';
 
 import { NoticeTagsList, NoticeTagItem } from '../../components';
 
-interface NoticeTagsListProps {
-  selectedTag: string;
-  NoticeActions: typeof noticeActions;
-}
+interface NoticeTagsListProps {}
 
-const NoticeTagsListContainer: React.FC<NoticeTagsListProps> = ({
-  selectedTag,
-  NoticeActions,
-}) => {
+const NoticeTagsListContainer: React.FC<NoticeTagsListProps> = () => {
+  const dispatch = useDispatch();
+  const selectedTag = useSelector(
+    (state: StoreState) => state.notice.selectedTag,
+  );
+
+  const setSelectedTag = (
+    tagId: string,
+    tableType: string,
+    staticTableIndex: number,
+  ) => {
+    return dispatch(setSelectedTagThunk(tagId, tableType, staticTableIndex));
+  };
+
   const NoticeTagItems = NoticeTags.map((data, index) => (
     <NoticeTagItem
       tagContent={data.tagContent}
       tableType={data.tableType}
       staticTableIndex={data.staticTableIndex}
       selectedTag={selectedTag}
-      onClickTag={NoticeActions.setSelectedTagThunk}
+      onClickTag={setSelectedTag}
       key={`notice tag - ${data.tagContent} - ${index}`}
     />
   ));
   return <NoticeTagsList>{NoticeTagItems}</NoticeTagsList>;
 };
 
-const mapStateToProps = state => ({
-  selectedTag: state.notice.selectedTag,
-});
-
-const mapDispatchToProps = dispatch => ({
-  NoticeActions: bindActionCreators(noticeActions, dispatch),
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(NoticeTagsListContainer);
+export default NoticeTagsListContainer;
