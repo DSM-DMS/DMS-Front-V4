736ec01d573dc39c77b40c640ca249cad71aa9d9
SuHaLee <gimtommang88@gmail.com>
2019-11-06 23:32:29 +0900

    [MODIFY]<src/Routing.tsx> 라우팅 설정 메인부분 추가함

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { NoticeContainer, MypageContainer, ApplyStayContainer, ApplyExtensionContainer } from './containers';
 
+import Meal from './components/Meal/Meal'
+
 interface RoutingProps {}
 
 const Routing: React.StatelessComponent<RoutingProps> = () => {
   return (
     <>
-      <Route path="/" component={() => <div>1</div>} exact />
+      <Route path="/" component={Meal} exact />
       <Route path="/etc/notice" component={NoticeContainer} exact />
       <Route path="/etc/mypage" component={MypageContainer} exact />
       <Route path="/apply/stay" component={ApplyStayContainer} exact />
