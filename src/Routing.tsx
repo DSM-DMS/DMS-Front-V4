51fc2df21216923eb1e63f1fa90d9f6731f7e854
SuHaLee <gimtommang88@gmail.com>
2019-11-28 22:25:11 +0900

    [ADD]<src/.Routing.tsx> Meal route 추가
    Meal부분 라우터에 추가

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { NoticeContainer, MypageContainer, ApplyStayContainer, ApplyExtensionContainer } from './containers';
+import {
+  NoticeContainer,
+  MypageContainer,
+  ApplyStayContainer,
+  ApplyExtensionContainer,
+} from './containers';
 
-import Meal from './components/Meal/Meal'
+import MealContainer from './containers/Meal/Meal';
 
 interface RoutingProps {}
 
 const Routing: React.StatelessComponent<RoutingProps> = () => {
   return (
     <>
-      <Route path="/" component={Meal} exact />
+      <Route path="/" component={MealContainer} exact />
       <Route path="/etc/notice" component={NoticeContainer} exact />
       <Route path="/etc/mypage" component={MypageContainer} exact />
       <Route path="/apply/stay" component={ApplyStayContainer} exact />
-      <Route path="/apply/extension" component={ApplyExtensionContainer} exact />
+      <Route
+        path="/apply/extension"
+        component={ApplyExtensionContainer}
+        exact
+      />
     </>
   );
 };
