c866e3f0d64929e1083ef129767459fb72c3a5d0
SuHaLee <gimtommang88@gmail.com>
2019-11-28 22:26:29 +0900

    [ADD]<src/components/Meal/Meal.tsx> Props 추가
    Meal component props 추가

--- src/components/Meal/Meal.tsx
+++ src/components/Meal/Meal.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import * as S from './style';
 import MealCard from '../MealCard/MealCard';
-import morning from "../../assets/icon/morning.png";
-import lunch from "../../assets/icon/lunch.png";
-import dinner from "../../assets/icon/dinner.png";
+import morning from '../../assets/icon/morning.png';
+import lunch from '../../assets/icon/lunch.png';
+import dinner from '../../assets/icon/dinner.png';
 
-interface Props{
+interface Props {
+  morning: any;
+  lunch: any;
+  dinner: any;
 }
 
-const Meal: React.StatelessComponent<Props>= () => {
+const Meal: React.StatelessComponent<Props> = ({ morning, lunch, dinner }) => {
   return (
     <>
       <S.MealContainer>
-        <MealCard IconSrc={morning} eatingTime={"아침"} />
-        <MealCard IconSrc={lunch} eatingTime={"점심"}/>
-        <MealCard IconSrc={dinner} eatingTime={"저녁"}/>
+        <MealCard IconSrc={morning} eatingTime={'아침'}></MealCard>
+        <MealCard IconSrc={lunch} eatingTime={'점심'} />
+        <MealCard IconSrc={dinner} eatingTime={'저녁'} />
       </S.MealContainer>
     </>
   );
