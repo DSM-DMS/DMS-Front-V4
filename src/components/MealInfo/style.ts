68a30caa876fcef7d90088e15ffb46820962bcfb
SuHaLee <gimtommang88@gmail.com>
2019-12-09 23:52:15 +0900

    [ADD]<src/components/Mealinfo/style.ts> add style

--- src/components/MealInfo/style.ts
+++ src/components/MealInfo/style.ts
@@ -1,1 +1,13 @@
-  
+import styled from 'styled-components';
+
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 2.875rem;
+`;
+
+export const InfoItem = styled.span`
+  margin-top: 1.1rem;
+  font-size: 1.375rem;
+  color: #59545d;
+`;
