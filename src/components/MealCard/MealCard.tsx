0ac9eec7cca213e37dc79b13eeac1c5fdcbaa51d
SuHaLee <gimtommang88@gmail.com>
2019-12-09 23:30:15 +0900

    [Modify]<src/component/meal/MealCard.tsx>  mod code style
    의미없는 공백 삭제, mealListProps 추가

--- src/components/MealCard/MealCard.tsx
+++ src/components/MealCard/MealCard.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import * as S from './style';
+import MealInfo from '..//MealInfo/MealInfo';
 
-interface Props{
-  IconSrc: any
-  eatingTime: String
+interface Props {
+  IconSrc: any;
+  eatingTime: String;
+  mealList: string[];
 }
 
-const MealCard: React.StatelessComponent<Props>= ({IconSrc,eatingTime}) => {
+const MealCard: React.StatelessComponent<Props> = ({ IconSrc, eatingTime, mealList }) => {
   return (
     <>
-     <S.MealCardContainer>
-       <S.MealCardHeader>
-         <S.MealCardIcon src={IconSrc}/>
-         <S.MealCardName>
-           {eatingTime}
-         </S.MealCardName>
-       </S.MealCardHeader>
-       <S.MealInfo>
-        
-       </S.MealInfo>
-     </S.MealCardContainer>
+      <S.MealCardContainer>
+        <S.MealCardHeader>
+          <S.MealCardIcon src={IconSrc} />
+          <S.MealCardName>{eatingTime}</S.MealCardName>
+        </S.MealCardHeader>
+        <MealInfo mealInfo={mealList}></MealInfo>
+      </S.MealCardContainer>
     </>
   );
 };
