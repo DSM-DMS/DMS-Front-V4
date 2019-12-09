6672eab1e4aedc856d361ce97e7eebe1558b7fab
SuHaLee <gimtommang88@gmail.com>
2019-12-09 23:30:55 +0900

    [Modify]<src/component/Mealcard/style.ts> Mod code style

--- src/components/MealCard/style.ts
+++ src/components/MealCard/style.ts
@@ -7,29 +7,25 @@ export const MealCardContainer = styled.div`
   background-color: #fbfbfb;
   border-radius: 1.875rem;
   flex-direction: column;
-  
-`
+`;
 
 export const MealCardHeader = styled.div`
   display: flex;
   height: 6.438rem;
-  width : 100%;
-  align-items: center; 
-`
+  width: 100%;
+  align-items: center;
+`;
 
 export const MealCardIcon = styled.img`
-  width:3.688rem;
-  height:3.75rem;
-  margin-left:2.125rem;
-`
+  width: 3.688rem;
+  height: 3.75rem;
+  margin-left: 2.125rem;
+`;
 
 export const MealCardName = styled.span`
   font-size: 1.25rem;
   color: #59545d;
   font-weight: 500;
   margin-left: 1.25rem;
-`
+`;
 
-export const MealInfo = styled.div`
-  font-size:1.375rem;
-`
