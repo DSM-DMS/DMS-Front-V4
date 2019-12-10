16b7e9c3b9ae013508bd2a9845ed4d783c9567a7
SuHaLee <gimtommang88@gmail.com>
2019-12-10 11:18:33 +0900

    [Modfiy]<src/container/Meal/meal.tsx> 더미값 수정, props수정
    더미값 실제 급식 값으로 수정,  props값 breakfast, lunch, dinner로 세분화해서 내려줌

--- src/containers/Meal/Meal.tsx
+++ src/containers/Meal/Meal.tsx
@@ -3,17 +3,41 @@ import Meal from '../../components/Meal/Meal';
 
 interface Props {}
 
-const mealList: Object = {
-  breakfast: ['밥', '국', '김치', '반찬1', '반찬2'],
-  dinner: ['밥', '국', '김치', '반찬1', '반찬2'],
-  lunch: ['밥', '국', '김치', '반찬1', '반찬2'],
-};
-
 const MealContainer: React.FC<Props> = () => {
-  const [mealInfo, SetMealInfo] = useState(mealList);
+  const mealList = {
+    breakfast: [
+      '현미밥',
+      '카래감자볶음',
+      '배추김치',
+      '맑은미역국',
+      '사과',
+      '쥬스',
+      '옥수수치즈닭불구이',
+    ],
+    lunch: [
+      '기장밥',
+      '콩비지찌개',
+      '돈육콩불이',
+      '해물볶음우동',
+      '열무김치',
+      '우리밀애그타르트',
+    ],
+    dinner: [
+      '흑미밥',
+      '등뼈우거지탕',
+      '햄에그까스',
+      '부들어묵볶음',
+      '깍두기',
+      '방울토마토',
+    ],
+  };
   return (
     <>
-      <Meal mealInfo={mealInfo}></Meal>
+      <Meal
+        breakfast={mealList.breakfast}
+        lunch={mealList.lunch}
+        dinner={mealList.dinner}
+      ></Meal>
     </>
   );
 };
