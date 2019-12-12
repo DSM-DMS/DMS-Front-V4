3bc9461a495dcf493d7a1a1d3042c47918f58b34
SuHaLee <gimtommang88@gmail.com>
2019-12-12 12:22:54 +0900

    [Add]<src/components/Meal/MealInfo.tsx, style.ts> 급식 길이에 따라 스타일 변경하는 코드 작성
    ref와 useEffect hooks를 사용해서 렌더링 후에 InfoContainer height를 받아 props로 내려줘서 특정 길이보다  작다면 일정한 간격을 주고, 길다면 따로 height을 줘서 space-between을 줘 div밖으로 넘치지 않게 처리하였습니다

--- src/components/MealInfo/MealInfo.tsx
+++ src/components/MealInfo/MealInfo.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import * as S from './style';
 
 interface Props {
   mealInfo: string[];
 }
 
-const MealInfo: React.StatelessComponent<Props> = ({ mealInfo }) => {
+const MealInfo: React.FC<Props> = ({ mealInfo }) => {
+  const [height, setHeight] = useState(0);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    setHeight(ref.current.clientHeight);
+  });
+
   return (
-    <S.InfoContainer>
+    <S.InfoContainer ref={ref} height={height}>
       {mealInfo.map(infoItem => (
-        <S.InfoItem>{infoItem}</S.InfoItem>
+        <S.InfoItem height={height}>{infoItem}</S.InfoItem>
       ))}
     </S.InfoContainer>
   );
--- src/components/MealInfo/style.ts
+++ src/components/MealInfo/style.ts
@@ -1,14 +1,27 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const InfoContainer = styled.div`
   display: flex;
-  flex-direction: column;
   margin-left: 2.875rem;
+  width: 14.13rem;
+  flex-direction: column;
+  ${props =>
+    props.height >= 210
+      ? css`
+    height: 19.63rem
+    justify-content: space-between
+    margin-top: 1rem;`
+      : null}
 `;
 
 export const InfoItem = styled.span`
-  margin-top: 1rem;
   font-size: 1.375rem;
   color: #59545d;
   font-weight: 400;
+  ${props =>
+    props.height <= 210
+      ? css`
+          margin-top: 1rem;
+        `
+      : null}
 `;
