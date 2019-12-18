b6e49c917b11948ed417ec576cdbe4e6c1e7b662
SuHaLee <gimtommang88@gmail.com>
2019-12-18 23:51:24 +0900

    [Create]<src/components/Calendar/Calendar.tsx, style.ts> create calendar component
    calendar컴포넌트 생성, 스타일 추가

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import * as S from './style';
+import CalendarItem from '../CalendarItem/CalendarItem';
 
-const Calendar = () => {
-  return <div></div>;
+interface Props {
+  date: number;
+}
+const Calendar: React.FC<Props> = ({ date }) => {
+  return (
+    <S.CalendarItemWrapper>
+      <CalendarItem day={'월'} date={date}></CalendarItem>
+      <CalendarItem day={'화'} date={date}></CalendarItem>
+      <CalendarItem day={'수'} date={date}></CalendarItem>
+      <CalendarItem day={'목'} date={date}></CalendarItem>
+      <CalendarItem day={'금'} date={date}></CalendarItem>
+      <CalendarItem day={'토'} date={date}></CalendarItem>
+      <CalendarItem day={'일'} date={date}></CalendarItem>
+    </S.CalendarItemWrapper>
+  );
 };
 
 export default Calendar;
--- src/components/Calendar/style.ts
+++ src/components/Calendar/style.ts
@@ -1,1 +1,9 @@
 import styled from 'styled-components';
+
+export const CalendarItemWrapper = styled.div`
+  display: flex;
+  width: 59.31rem;
+  height: 9.688rem;
+  justify-content: space-between;
+  margin-left: 7.813rem;
+`;
