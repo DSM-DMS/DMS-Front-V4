b96562ce0ba9f35964336018a2aeafa6c3ba3506
SuHaLee <gimtommang88@gmail.com>
2019-12-18 15:53:58 +0900

    [Create]<src/component/AcademicCalender/AcademicCalender.tsx, style.ts>
    학사일정 컴포넌트 생성

---
+++ src/components/AcademicCalendar/AcademicCalendar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import * as S from './style';
+
+interface Props {}
+const AcademicCalendar: React.FC<Props> = () => {
+  return <S.AcademicCalendarContainer />;
+};
+
+export default AcademicCalendar;
---
+++ src/components/AcademicCalendar/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const AcademicCalendarContainer = styled.div`
+  width: 61.88rem;
+  height: 10.88rem;
+  border: solid 0.1875rem #e3e3e3;
+  border-radious: 7.5rem/ 0px 6.25rem 6.25rem;
+`;
