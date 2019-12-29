986c8eed7bb0b7bcffbb7ffba7251d5186caf039
SuHaLee <gimtommang88@gmail.com>
2019-12-29 23:16:42 +0900

    [ADD]<src/component/AcademicCalendar/AcademicCalendar.tsx> <src/component/AcademicCalendarItem/AcademicCalendarItem.tsx>  학사일정 데이터 보여주는 로직 추가
    학사일정 데이터 불러와서 AcademicCalendarItem에 테이블 형태로 보여주는 로작 추가.

--- src/components/AcademicCalendar/AcademicCalendar.tsx
+++ src/components/AcademicCalendar/AcademicCalendar.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import * as S from './style';
+import AcademicCalendarItem from '../AcdemiCalendarItem/AcademicCalendarItem';
 
-interface Props {}
-const AcademicCalendar: React.FC<Props> = () => {
-  return <S.AcademicCalendarContainer />;
+interface Props {
+  AcademicCalendarData: { scadule: string; date: string; place: string }[];
+}
+const AcademicCalendar: React.StatelessComponent<Props> = ({
+  AcademicCalendarData,
+}) => {
+  return (
+    <S.AcademicCalendarWrapper>
+      <div style={{ overflow: 'auto' }}>
+        <S.AcademicCalendarTabel>
+          {AcademicCalendarData.map(dataItem => (
+            <AcademicCalendarItem
+              scadule={dataItem.scadule}
+              date={dataItem.date}
+              place={dataItem.place}
+            />
+          ))}
+        </S.AcademicCalendarTabel>
+      </div>
+    </S.AcademicCalendarWrapper>
+  );
 };
 
 export default AcademicCalendar;
--- src/components/AcdemiCalendarItem/AcademicCalendarItem.tsx
+++ src/components/AcdemiCalendarItem/AcademicCalendarItem.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import * as S from './style';
 
-export interface Props {}
+export interface Props {
+  scadule: string;
+  date: string;
+  place: string;
+}
 
-const AcademicCalendarItem: React.StatelessComponent<Props> = () => {
+const AcademicCalendarItem: React.StatelessComponent<Props> = ({
+  scadule,
+  date,
+  place,
+}) => {
   return (
     <S.AcademicCalendarItemRow>
-      <S.AcademicCalendarItemData>
-        정보보안과 대전교육정보원 체헙학습
-      </S.AcademicCalendarItemData>
-      <S.AcademicCalendarItemData>
-        오전 09:00~오후12:00
-      </S.AcademicCalendarItemData>
-      <S.AcademicCalendarItemData>대전교육정보원</S.AcademicCalendarItemData>
+      <S.AcademicCalendarItemData>{scadule}</S.AcademicCalendarItemData>
+      <S.AcademicCalendarItemData>{date}</S.AcademicCalendarItemData>
+      <S.AcademicCalendarItemData>{place}</S.AcademicCalendarItemData>
     </S.AcademicCalendarItemRow>
   );
 };
