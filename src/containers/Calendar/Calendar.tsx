f1278ff0e86d5548ccb144b6f8d3015897f8c4ad
SuHaLee <gimtommang88@gmail.com>
2020-01-04 22:30:22 +0900

    [ADD]<src/containers/Calendar/Calendar.tsx>일주일 날짜 배열로 보여주는 함수 생성
    파라미터로 날짜를 받은 후 , 그 날이 있는 주의 배열을 return 해주는 함수 생성

--- src/containers/Calendar/Calendar.tsx
+++ src/containers/Calendar/Calendar.tsx
@@ -1,11 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Calendar from '../../components/Calendar/Calendar';
+import moment, { weekdays, Moment } from 'moment';
 
 interface Props {}
 
 const CalendarContainer: React.FC<Props> = () => {
-  const [date, setDate] = useState<number>(0);
-  return <Calendar date={12} />;
+  const [weekDate, setWeekDate] = useState<string[]>([]);
+  const today: string = moment().format('YYYY MM DD');
+
+  const startOfDay = (date: string): Moment => {
+    const startDate: Moment = moment(date).startOf('week');
+    return startDate;
+  };
+  const endOfDay = (date: string): Moment => {
+    const endDate: Moment = moment(date).endOf('week');
+    return endDate;
+  };
+
+  const week = (today: string): string[] => {
+    const newDate: string[] = [];
+    const day = startOfDay(today);
+    console.log(
+      `day : ${day.format('YYYY MM DD')} endDay : ${endOfDay(today).format(
+        `YYYY MM DD`,
+      )}`,
+    );
+    while (day <= endOfDay(today)) {
+      day.add(1, 'day');
+      console.log(`day : ${day.format('YYYY MM DD')}`);
+      newDate.push(day.format('YYYY MM DD'));
+      console.log(newDate);
+    }
+    console.log(`final newDate : ${newDate}`);
+    return newDate;
+  };
+
+  useEffect(() => {
+    week(today);
+  });
+
+  return (
+    <>
+      <Calendar date={12} />
+    </>
+  );
 };
 
 export default CalendarContainer;
