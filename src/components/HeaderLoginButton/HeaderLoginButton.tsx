8200d60c72852ce3772cf2af20975b4b72baa8be
SuHaLee <gimtommang88@gmail.com>
2020-03-09 22:48:08 +0900

    <src/components/HeaderLoginButton/HeaderLoginButton.tsx, style.ts> create header login button

---
+++ src/components/HeaderLoginButton/HeaderLoginButton.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as S from './style';
+
+const HeaderLoginButton = () => {
+  return <S.OpenLoginModalButton />;
+};
+
+export default HeaderLoginButton;
---
+++ src/components/HeaderLoginButton/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const OpenLoginModalButton = styled.button`
+  font-size: 1.25rem;
+  color: #59545d;
+  background-color: transparent;
+  margin-right: 3.063rem;
+`;
