1625e3d8a834e2188e1b414756e1ee060a1761b8
SuHaLee <gimtommang88@gmail.com>
2020-03-12 23:52:38 +0900

    [Create]<src/components/Modals/BugReport/style.ts> create bugReport style

---
+++ src/components/Modals/BugReport/style.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const BugreportInput = styled.textarea`
+  resize: none;
+  width: 23rem;
+  height: 19.94rem;
+  -webkit-appearance: none;
+  border: 0.0625rem solid #bbbbb;
+  border-radius: 0.9375rem;
+  font-family: inherit;
+  font-size: inherit;
+  padding-top: 1.5rem;
+  padding-left: 1.438rem;
+  padding-right: 1.438rem;
+`;
