c0c3f4b72ff502aa91515d2311506a754240290e
SuHaLee <gimtommang88@gmail.com>
2020-03-16 04:02:35 +0900

    [ADD]<src/components/Modals/ModalWrapper/ModalWrapper.tsx>> add props
    contaier로 통해 받아오는 props 사용

--- src/components/Modals/ModalWrapper/ModalWrapper.tsx
+++ src/components/Modals/ModalWrapper/ModalWrapper.tsx
@@ -3,25 +3,29 @@ import * as S from './style';
 import CloseImg from '../../../assets/icon/black-close-button.png';
 
 interface Props {
+  isVisible: boolean;
   modalName: string;
   children: ReactNode;
+  setModalHide: () => void;
 }
 
 const ModalWrapper: React.StatelessComponent<Props> = ({
   modalName,
   children,
+  isVisible,
+  setModalHide,
 }) => {
   return (
-    <>
-      <S.ModalOverlay />
+    <S.ModalWrapper isVisible={isVisible}>
+      <S.ModalOverlay onClick={setModalHide} />
       <S.ModalContainer>
         <S.ModalNameWrapper>
           <S.ModalName>{modalName}</S.ModalName>
-          <S.ModalClosebtn src={CloseImg} />
+          <S.ModalClosebtn src={CloseImg} onClick={setModalHide} />
         </S.ModalNameWrapper>
         {children}
       </S.ModalContainer>
-    </>
+    </S.ModalWrapper>
   );
 };
 
