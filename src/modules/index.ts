ff921434b40b15b8c869951a32834a5abcdffd7f
SuHaLee <gimtommang88@gmail.com>
2020-03-16 18:11:49 +0900

    [ADD]<src/module/index.ts> add modal

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import notice, { NoticeState } from './notice';
 import chat, { ChatState } from './chat';
+import modal, { ModalState } from './modal';
 
 export interface StoreState {
   notice: NoticeState;
   chat: ChatState;
+  modal: ModalState;
 }
 
-export default combineReducers<StoreState>({ notice, chat });
+export default combineReducers<StoreState>({ notice, chat, modal });
