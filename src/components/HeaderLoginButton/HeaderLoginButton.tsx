bf75c5c000aab0642361c20a57c4653e14a1b701
SuHaLee <gimtommang88@gmail.com>
2020-03-16 18:14:07 +0900

    [ADD]<src/components/HeaderLoginButton/HeaderLogInButton.tsx> add changeModalType click event

--- src/components/HeaderLoginButton/HeaderLoginButton.tsx
+++ src/components/HeaderLoginButton/HeaderLoginButton.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import * as S from './style';
+import { modalTypes } from '../../modules/modal';
 
-const HeaderLoginButton = () => {
-  return <S.OpenLoginModalButton>로그인</S.OpenLoginModalButton>;
+interface Props {
+  changeModalType: (modalType: modalTypes) => void;
+}
+const HeaderLoginButton: React.StatelessComponent<Props> = ({
+  changeModalType,
+}) => {
+  return (
+    <S.OpenLoginModalButton onClick={() => changeModalType(modalTypes.LogIn)}>
+      로그인
+    </S.OpenLoginModalButton>
+  );
 };
 
 export default HeaderLoginButton;
