1dfd8f79def1982aff395ef8d38a5d7137f1c9cf
SuHaLee <gimtommang88@gmail.com>
2020-03-18 01:19:10 +0900

    [ADD]<src/components/Modals/LogIn/LogIn.tsx> add changeModalType func

--- src/components/Modals/Login/Login.tsx
+++ src/components/Modals/Login/Login.tsx
@@ -2,9 +2,12 @@ import * as React from 'react';
 import * as S from './style';
 import { ModalWrapper, ModalButton, ModalInput } from '../../../components';
 import { ModalWrapperContainer } from '../../../containers';
-interface Props {}
+import { ModalTypes } from '../../../modules/modal';
+interface Props {
+  changeModalType: (modalType: ModalTypes) => void;
+}
 
-const LogIn: React.StatelessComponent<Props> = () => {
+const LogIn: React.StatelessComponent<Props> = ({ changeModalType }) => {
   return (
     <ModalWrapperContainer modalName="로그인">
       <S.LogInContnetWrapper>
@@ -15,13 +18,17 @@ const LogIn: React.StatelessComponent<Props> = () => {
             <S.AutoLoginCheckbtn type="checkbox" />
             <span>자동 로그인</span>
           </S.AutoLoginWrapper>
-          <S.ForgetPWLink>비밀번호 찾기</S.ForgetPWLink>
+          <S.ForgetPWLink onClick={() => changeModalType(ModalTypes.ChangePW)}>
+            비밀번호 찾기
+          </S.ForgetPWLink>
         </S.LoginBottom>
       </S.LogInContnetWrapper>
       <ModalButton buttonName={'로그인'} />
       <S.SignInLinkWrapper>
         <span>아직 DMS 회원이 아니시라면?</span>
-        <S.SignInLink>회원가입</S.SignInLink>
+        <S.SignInLink onClick={() => changeModalType(ModalTypes.SignIn)}>
+          회원가입
+        </S.SignInLink>
       </S.SignInLinkWrapper>
     </ModalWrapperContainer>
   );
