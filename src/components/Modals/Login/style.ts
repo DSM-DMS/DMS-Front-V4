ed678a01f9b0c3f4d2632c4ab4382a711ea276a4
SuHaLee <gimtommang88@gmail.com>
2020-03-26 02:20:18 +0900

    [Modify]<src/components/Modals/LogIn/style.ts> extract common css
    자주 사용되는 css를 따로 빼서 Centerize로 만들어 사용함

--- src/components/Modals/Login/style.ts
+++ src/components/Modals/Login/style.ts
@@ -1,6 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { globalColor } from '../../../GlobalStyle';
 
+const Centerize = css`
+  display: flex;
+  align-items: center;
+`;
 export const LogInContnetWrapper = styled.div`
   display: flex;
   width: 24.75rem;
@@ -10,44 +14,41 @@ export const LogInContnetWrapper = styled.div`
 `;
 
 export const LoginBottom = styled.div`
-  display: flex;
+  ${Centerize}
   justify-content: space-between;
-  align-items: center;
 `;
 export const AutoLoginWrapper = styled.div`
-  display: flex;
-  align-items: center;
+  ${Centerize}
   justify-content: space-between;
 `;
 
-export const AutoLoginCheckbtn = styled.input`
-  display: flex
+export const AutoLoginCheckbtn = styled.input` 
+  ${Centerize}
   width: 2.25rem;
   height: 1.188rem;
   margin-right: 0.4375rem;
   appearance: none;
-  align-items: center;
-  background-color:#DCDCDC;
+  background-color: #dcdcdc;
   box-shadow: 0 0.1875rem 0.375rem 0 rgba(0, 0, 0, 0.16);
   border-radius: 1rem;
   cursor: pointer;
-  &:checked{
-    background-color: ${globalColor} ;
-    &::after{
-      margin-left:1.25rem;
-    } 
+  &:checked {
+    background-color: ${globalColor};
+    &::after {
+      margin-left: 1.25rem;
+    }
   }
-  &::after{
-    content: "";  
+  &::after {
+    content: '';
     margin-left: 0.1875rem;
     display: block;
     width: 0.8125rem;
     height: 0.8125rem;
     background-color: #fff;
     border-radius: 50%;
-    box-shadow: 0 0.1875rem 0.375rem 0 rgba(0, 0, 0, 0.16);   
+    box-shadow: 0 0.1875rem 0.375rem 0 rgba(0, 0, 0, 0.16);
     transition: 0.5s;
-  } 
+  }
 `;
 
 export const ForgetPWLink = styled.span`
@@ -55,14 +56,13 @@ export const ForgetPWLink = styled.span`
 `;
 
 export const SignInLinkWrapper = styled.div`
-  display: flex;
+  ${Centerize}
   width: 100%;
   height: 3.75rem
   border-radius: 0 0 1.455rem 1.455rem;
   font-size: 0.875rem;
   background-color: #eeefe7;  
   justify-content: center;
-  align-items: center;
 `;
 
 export const SignInLink = styled.span`
