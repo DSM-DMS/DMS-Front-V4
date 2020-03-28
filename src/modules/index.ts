b7b8159fe6013dac843bc81d78cb9646ec75fc7d
SuHaLee <gimtommang88@gmail.com>
2020-03-29 00:01:46 +0900

    [ADD]<src/modules/index.ts> add calendar module

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import notice, { NoticeState } from './notice';
 import chat, { ChatState } from './chat';
+import calendar, { CalendarState } from './calendar';
 
 export interface StoreState {
   notice: NoticeState;
   chat: ChatState;
+  calendar: CalendarState;
 }
 
-export default combineReducers<StoreState>({ notice, chat });
+export default combineReducers<StoreState>({ notice, chat, calendar });
