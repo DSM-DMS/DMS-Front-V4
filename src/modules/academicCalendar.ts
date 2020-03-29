62073eacc60625ccd87f670775ef8e014f6afb2c
SuHaLee <gimtommang88@gmail.com>
2020-03-30 04:09:49 +0900

    [Create]<src/modules/academicCalendar.ts> create academicCalendar redux module
    redux thunk를 이용한 academicCaelndar 리덕스 모듈 생성

---
+++ src/modules/academicCalendar.ts
@@ -0,0 +1,96 @@
+import { ThunkAction } from 'redux-thunk';
+import { getRequest } from '../util/api/api';
+import { AxiosError } from 'axios';
+
+const GET_SCHEDULE = 'GET_SCHEDULE' as const;
+const GET_SCHEDULE_SUCCESS = 'GET_SCHEDULE_SUCCESS' as const;
+const GET_SCHEDULE_ERROR = 'GET_SCHEDULE_ERROR' as const;
+
+const getSchedule = () => ({
+  type: GET_SCHEDULE,
+});
+const getScheduleSuccess = (schedule: {}) => ({
+  type: GET_SCHEDULE_SUCCESS,
+  payload: schedule,
+});
+
+const getScheduleError = (err: AxiosError) => ({
+  type: GET_SCHEDULE_ERROR,
+  payload: err,
+});
+
+type AcademicCalendarAction =
+  | ReturnType<typeof getSchedule>
+  | ReturnType<typeof getScheduleSuccess>
+  | ReturnType<typeof getScheduleError>;
+
+export function getScheduleThunk(
+  day: string,
+): ThunkAction<
+  Promise<void>,
+  AcademicCalendarState,
+  null,
+  AcademicCalendarAction
+> {
+  return async dispatch => {
+    dispatch({ type: GET_SCHEDULE });
+    try {
+      const response = await getRequest('/info/schedule/', day);
+      dispatch(getScheduleSuccess(response.data));
+    } catch (e) {
+      dispatch(getScheduleError(e));
+    }
+  };
+}
+
+export type scheduleType = {
+  schedule: [
+    {
+      name: string;
+      time: string;
+      place: string;
+    },
+  ];
+};
+
+export interface AcademicCalendarState {
+  data: any | null;
+  error: any | null;
+}
+
+const initialState: AcademicCalendarState = {
+  data: {
+    schedule: [
+      {
+        name: '정보보안과 대전교육정보원 체험학습',
+        time: '오전 09:00 ~ 오후 12:00',
+        place: '대전교육정보원',
+      },
+    ],
+  },
+  error: null,
+};
+
+function academicCalendar(
+  state = initialState,
+  action: AcademicCalendarAction,
+): AcademicCalendarState {
+  switch (action.type) {
+    case GET_SCHEDULE:
+      return {
+        ...state,
+      };
+    case GET_SCHEDULE_SUCCESS:
+      return {
+        ...state,
+        data: action.payload,
+      };
+    case GET_SCHEDULE_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+      };
+  }
+}
+
+export default academicCalendar;
