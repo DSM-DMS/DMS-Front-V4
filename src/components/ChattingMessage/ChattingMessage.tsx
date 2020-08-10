7e73050d150a22db9105e49e5468c1f1f6f9b654
gimtommang11 <gimtommang88@gmail.com>
2020-08-10 16:15:29 +0900

    add Chatting Sent Time component

--- src/components/ChattingMessage/ChattingMessage.tsx
+++ src/components/ChattingMessage/ChattingMessage.tsx
@@ -8,9 +8,21 @@ interface Props {
 
 const ChattingMessage: React.FC<Props> = ({ isSentMessage, message }) => {
   return (
-    <S.ChattingMessage isSentMessage={isSentMessage}>
-      <div>{message}</div>
-    </S.ChattingMessage>
+    <S.ChattingMessageWrapper isSentMessage={isSentMessage}>
+      {isSentMessage && (
+        <S.ChattingSentTime isSentMessage={isSentMessage}>
+          23:00
+        </S.ChattingSentTime>
+      )}
+      <S.ChattingMessage isSentMessage={isSentMessage}>
+        <div>{message}</div>
+      </S.ChattingMessage>
+      {!isSentMessage && (
+        <S.ChattingSentTime isSentMessage={isSentMessage}>
+          23:00
+        </S.ChattingSentTime>
+      )}
+    </S.ChattingMessageWrapper>
   );
 };
 
--- src/components/ChattingMessage/styles.ts
+++ src/components/ChattingMessage/styles.ts
@@ -1,34 +1,33 @@
 import styled from 'styled-components';
 
+export const ChattingMessageWrapper = styled.div<{ isSentMessage: boolean }>`
+  ${(props) =>
+    props.isSentMessage
+      ? `float: right;
+        margin-right: 1.688rem;`
+      : `color:#000000;
+      margin-left: 1.688rem;`}
+`;
+
 export const ChattingMessage = styled.div<{ isSentMessage: boolean }>`
   position: relative;
   display: inline-block;
   ${(props) =>
     props.isSentMessage
       ? `background-color: #00be91;
-  color: #fff;
-  float: right;
-  margin-right: 1.688rem;
-  `
+  color: #fff;`
       : `background-color: #eeefe7;
-  color:#000000;
-  margin-left: 1.688rem;
-`}
+  color:#000000;`}
   margin-top: 2.25rem;
   max-width: 19.38rem;
   border-radius: 10px;
   padding: 0.8125rem 0.625rem;
   font-size: 1.125rem;
-  // &:after {
-  //   position: relative;
-  //   content: '';
-  //   border-style: solid;
-  //   border-width: 5px 20px 5px 0;
-  //   border-color: transparent red;
-  //   display: block;
-  //   width: 0;
-  //   z-index: 1;
-  //   left: -20px;
-  //   top: 6px;
-  // }
+`;
+
+export const ChattingSentTime = styled.span<{ isSentMessage: boolean }>`
+  font-size: 0.8125rem;
+  color: #686868;
+  ${(props) =>
+    props.isSentMessage ? `margin-right: 0.4375rem` : `margin-left: 0.4375rem;`}
 `;
