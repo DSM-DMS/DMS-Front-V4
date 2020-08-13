8751fb2bda5884f854db816c62849771b8fbb0c7
gimtommang11 <gimtommang88@gmail.com>
2020-08-13 09:26:04 +0900

    create meal redux

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -3,12 +3,20 @@ import notice, { NoticeState } from './notice';
 import chat, { ChatState } from './chat';
 import calendar, { CalendarState } from './calendar';
 import modal, { ModalState } from './modal';
+import meal, { MealState } from './meal';
 
 export interface StoreState {
   notice: NoticeState;
   chat: ChatState;
   calendar: CalendarState;
   modal: ModalState;
+  meal: MealState;
 }
 
-export default combineReducers<StoreState>({ notice, chat, modal, calendar });
+export default combineReducers<StoreState>({
+  notice,
+  chat,
+  modal,
+  calendar,
+  meal,
+});
---
+++ src/modules/meal.ts
@@ -0,0 +1,70 @@
+import { getMealReq } from '../util/api/api';
+import * as apiTypes from '../util/api/apiTypes';
+import { Dispatch } from 'react';
+import { ThunkAction } from 'redux-thunk';
+import { StoreState } from 'modules';
+
+export const GET_MEAL = 'GET_MEAL' as const;
+export const GET_MEAL_SUCCESS = 'GET_MEAL_SUCCESS' as const;
+export const GET_MEAL_FAILURE = 'GET_MEAL_FAILURE' as const;
+
+export const getMeal = () => ({
+  type: GET_MEAL,
+});
+
+export const getMealSuccess = (meal: apiTypes.mealType) => ({
+  type: GET_MEAL_SUCCESS,
+  payload: meal,
+});
+
+export const getMealFailure = (err) => ({
+  type: GET_MEAL_FAILURE,
+  payload: err,
+});
+
+export const getMealThunk = (
+  date: string,
+): ThunkAction<void, StoreState, null, MealAction> => async (dispatch) => {
+  dispatch(getMeal());
+  try {
+    const response = await getMealReq(date);
+    dispatch(getMealSuccess(response[0]));
+  } catch (err) {
+    dispatch(getMealFailure(err.status));
+  }
+};
+
+type MealAction =
+  | ReturnType<typeof getMeal>
+  | ReturnType<typeof getMealSuccess>
+  | ReturnType<typeof getMealFailure>;
+
+export interface MealState {
+  meal: apiTypes.mealType | null;
+  errorStatus: any | null;
+}
+
+const initialState: MealState = {
+  meal: {
+    breakfast: [],
+    lunch: [],
+    dinner: [],
+  },
+  errorStatus: null,
+};
+
+export default function meal(
+  state = initialState,
+  action: MealAction,
+): MealState {
+  switch (action.type) {
+    case GET_MEAL:
+      return { ...state };
+    case GET_MEAL_SUCCESS:
+      return { ...state, meal: action.payload };
+    case GET_MEAL_FAILURE:
+      return { ...state, errorStatus: action.payload };
+    default:
+      return { ...state };
+  }
+}
