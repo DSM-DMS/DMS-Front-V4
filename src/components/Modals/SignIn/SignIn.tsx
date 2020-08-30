52d131c92205ecc0e5332cbad2d263eb4a8ca532
gimtommang11 <gimtommang88@gmail.com>
2020-08-30 23:53:04 +0900

    ADD verifyCodeInput

--- src/components/Modals/SignIn/SignIn.tsx
+++ src/components/Modals/SignIn/SignIn.tsx
@@ -1,19 +1,58 @@
 import * as React from 'react';
+
 import * as S from './style';
+import * as apiTypes from '../../../util/apiTypes';
 import { ModalWrapper, ModalButton, ModalInput } from '../../../components';
 import { ModalWrapperContainer } from '../../../containers';
-interface Props {}
 
-const SignIn: React.StatelessComponent<Props> = () => {
+interface Props {
+  handleSignIn: (signInParams: apiTypes.signUpParamType) => void;
+}
+
+const SignIn: React.FC<Props> = ({ handleSignIn }) => {
+  const [signInInput, setSignInInput] = React.useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    { verifyCode: '', id: '', password: '', checkPassword: '' },
+  );
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const newValue = e.target.value;
+    setSignInInput({ [name]: newValue });
+  };
+
   return (
     <ModalWrapperContainer modalName="회원가입">
       <S.SignInContentWrapper>
-        <ModalInput placeholder="확인코드" />
-        <ModalInput placeholder="아이디" />
-        <ModalInput placeholder="비밀번호" inputType="password" />
-        <ModalInput placeholder="비밀번호 확인" inputType="password" />
+        <S.VerifyCodeInputWrapper>
+          <S.VerifyCodeInput
+            placeholder="확인코드 입력"
+            name="checkCode"
+            value={signInInput.checkCode}
+            onChange={handleChange}
+          />
+          <S.VerifyCodeButton
+            onClick={() => console.log(signInInput.verifyCode)}
+          >
+            인증
+          </S.VerifyCodeButton>
+        </S.VerifyCodeInputWrapper>
+        <ModalInput placeholder="아이디" value={signInInput.id} />
+        <ModalInput
+          placeholder="비밀번호"
+          inputType="password"
+          value={signInInput.password}
+        />
+        <ModalInput
+          placeholder="비밀번호 확인"
+          inputType="password"
+          value={signInInput.checkPassword}
+        />
       </S.SignInContentWrapper>
-      <ModalButton buttonName="회원가입" />
+      <ModalButton
+        buttonName="회원가입"
+        // onClick={() => handleSignIn({ })}
+      />
     </ModalWrapperContainer>
   );
 };
--- src/components/Modals/SignIn/style.ts
+++ src/components/Modals/SignIn/style.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { globalColor } from '../../../GlobalStyle';
 
 export const SignInContentWrapper = styled.div`
   display: flex;
@@ -6,4 +7,36 @@ export const SignInContentWrapper = styled.div`
   flex-direction: column;
   justify-content: space-between;
   height: 16.5rem;
+`;
+
+export const VerifyCodeInputWrapper = styled.div`
+  display: flex;
+  height: 3.125rem;
+  border-radius: 0.9375rem;
+  padding: 0 1.125rem ;
+  background-color: #fff;
+  border: 0.0625rem solid #bbbbbb;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between
+  &:focus {
+    border-color: ${globalColor};
+  }
+`;
+
+export const VerifyCodeInput = styled.input`
+  width: 19rem;
+  line-height: normal;
+  font-size: 1rem;
+  border: none;
+`;
+
+export const VerifyCodeButton = styled.button`
+  width: 3.625rem;
+  height: 1.813rem;
+  border-radius: 0.7rem;
+  border: none;
+  background-color: #00be91;
+  font-size: 0.5rem;
+  color: #fff;
 `;
