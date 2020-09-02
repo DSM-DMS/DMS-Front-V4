6f572501d7aae9609626a28637f5cdf4f71ce0e0
gimtommang11 <gimtommang88@gmail.com>
2020-09-02 18:17:10 +0900

    add props in modalInput

--- src/components/Modals/ModalInput/ModalInput.tsx
+++ src/components/Modals/ModalInput/ModalInput.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as S from './style';
 
 interface Props {
   placeholder: string;
+  inputName: string;
   inputType?: string;
+  value: string | number;
+  changeEvent?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const ModalInput: React.StatelessComponent<Props> = ({
+const ModalInput: React.FC<Props> = ({
   placeholder,
   inputType,
+  inputName,
+  value,
+  changeEvent,
 }) => {
-  return <S.ModalInput placeholder={placeholder} type={inputType} />;
+  return (
+    <S.ModalInput
+      placeholder={placeholder}
+      type={inputType}
+      name={inputName}
+      value={value}
+      onChange={changeEvent}
+      onFocus={() => console.log(value)}
+    />
+  );
 };
 
 export default ModalInput;
