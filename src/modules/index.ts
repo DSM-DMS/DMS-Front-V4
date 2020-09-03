d4f25a5a065e91661fb1d055a9eee2c8b51caf5d
gimtommang11 <gimtommang88@gmail.com>
2020-09-03 22:29:53 +0900

    add signup redux module in index.js

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import notice, { NoticeState } from './notice';
 import chat, { ChatState } from './chat';
 import modal, { ModalState } from './modal';
+import signUp, { SignUpState } from './signUp';
 
 export interface StoreState {
   notice: NoticeState;
   chat: ChatState;
   modal: ModalState;
+  signUp: SignUpState;
 }
 
-export default combineReducers<StoreState>({ notice, chat, modal });
+export default combineReducers<StoreState>({ notice, chat, modal, signUp });
